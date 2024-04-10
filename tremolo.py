2ec64adab875efaba93169d6718ae5ed92f28ec2
Jonathan García-Mallén <jonathangarciamallen@gmail.com>
2024-04-10 13:47:39 -0400

    Close thread upon window close. Add waveforms.
    I think what's next at this point is an icon and a .desktop file!

--- tremolo.py
+++ tremolo.py
@@ -4,7 +4,7 @@ import sys
 from typing import Final
 
 import mido
-from numpy import linspace, pi, cos
+from numpy import cos, linspace, pi, sqrt, square
 
 import gi
 
@@ -19,22 +19,21 @@ it again."""
 
 
 # Series of trim level values. Default is Triangle wave.
-# Sine as it is here matches up well with triangle.
+# Sine as it is here matches up well with triangle. Apologies for my dishonesty in using cosine.
+# Hill and Valley use the equation for a circle. Parabola could have worked as well and is simpler, but is more similar to sine. Circle is more distinct, so I feel it's better than parabola.
 WAVEFORM_SERIES: Final = {
     "triangle": tuple(range(127)) + tuple(range(127, 0, -1)),
     "square": tuple([0] * 127 + [127] * 127),
     "sine": tuple(map(round, cos(linspace(-pi, pi, 254)) * 64 + 64)),
     "sawtooth": tuple(map(round, linspace(0, 127, 254))),
+    "reverse sawtooth": tuple(map(round, linspace(127, 0, 254))),
+    "hill": tuple(map(round, sqrt(1 - square(linspace(-1, 1, 254))) * 127)),
+    "valley": tuple(map(round, (1 - sqrt(1 - square(linspace(-1, 1, 254)))) * 127)),
+    # todo: add random
 }
 
-# idk if python will let me change the array I'm iterating over, but that's what I
-# plan on doing. In C I'd have no trouble so long as the length stays the same.
-tril, sqrl, sinl, sawl = map(len, WAVEFORM_SERIES.values())
-assert tril == sqrl == sinl == sawl
-
-# Cast to list for mutation by depth
-# This must be the only assignment to series. Yes, it's global. Both classes need to see it. GUI alone modifies it.
-series = list(WAVEFORM_SERIES["triangle"])
+# Lenths of all series must be the same
+assert len(set(map(len, WAVEFORM_SERIES.values()))) == 1
 
 
 def bpm2period(bpm):
@@ -42,10 +41,13 @@ def bpm2period(bpm):
     message period in seconds."""
     hz = bpm / 60
     period = 1 / hz
-    return period / len(series)
+    return period / len(WAVEFORM_SERIES["triangle"])
 
 
-period_and_state = [bpm2period(1), True]  # There is a better way, beyond my knowledge.
+# Objects for cross-thread communication. They can only be assigned to once: here. GUI alone modifies these.
+# Cast to list for mutation by depth
+series = list(WAVEFORM_SERIES["triangle"])
+period_and_state = [bpm2period(1), True] # There is a better way, somewhere beyond my knowledge.
 
 
 class GUI(Gtk.ApplicationWindow):
@@ -62,9 +64,8 @@ class GUI(Gtk.ApplicationWindow):
         self.set_child(self.box1)  # Horizontal box to window
 
         # Jonathan attempts to add a drop down
-        self.waveform_dropdown = Gtk.DropDown.new_from_strings(
-            ["triangle", "square", "sine", "sawtooth"]
-        )
+        self.waveform_dropdown = Gtk.DropDown.new_from_strings(tuple(WAVEFORM_SERIES.keys()))
+
         # https://discourse.gnome.org/t/example-of-gtk-dropdown-with-search-enabled-without-gtk-expression/12748
         self.waveform_dropdown.connect("notify::selected-item", self.waveform_selected)
         self.box1.append(self.waveform_dropdown)
@@ -138,7 +139,7 @@ class GUI(Gtk.ApplicationWindow):
     def waveform_selected(self, dropdown, data):
         # https://discourse.gnome.org/t/example-of-gtk-dropdown-with-search-enabled-without-gtk-expression/12748
         selection = dropdown.get_selected_item().get_string()  # this API feels inane
-        # An assignment seems to create a new list object. I need to keep the same obj.
+        # An assignment seems to create a new list object. I need to keep the same obj for thread communication.
         for i in range(len(series)):
             series[i] = WAVEFORM_SERIES[selection][i]
         self.apply_depth()
@@ -155,28 +156,26 @@ class MyApp(Adw.Application):
         if not self.debug:
             inport = mido.open_input("Source Audio EQ2 MIDI 1")  # todo: try removing this line
             self.outport = mido.open_output("Source Audio EQ2 MIDI 1")
-            self.set_trim = lambda v: mido.Message(
-                "control_change", channel=0, control=2, value=v
-            )
+            self.set_trim = lambda v: mido.Message("control_change", channel=0, control=2, value=v)
         else:
             self.set_trim = lambda v: print("Channel 2 Trim set to ", v)
 
         # todo: stop thread once the window is closed
-        thread = threading.Thread(target=self.run_tremolo)
-        thread.start()
+        self.closing = False
+        self.thread = threading.Thread(target=self.run_tremolo)
+        self.thread.start()
 
     def on_activate(self, app):
         self.win = GUI(application=app)
         self.win.present()
 
     def run_tremolo(self):
-        # user must ^C
-        while True:
+        while not self.closing:
             # period_and_state is a 2-list, for thread reasons.
             # period is the MIDI CC period between messages: a number.
             # state is whether or not the effect is engaged: boolean.
             if period_and_state[1]:
-                has_been_shut_down = False
+                has_been_reset = False
                 for level in series:
                     if period_and_state[1]:
                         if self.debug:
@@ -186,11 +185,23 @@ class MyApp(Adw.Application):
                         time.sleep(period_and_state[0])
                     elif not period_and_state[1]:
                         break
+
+                    if self.closing:
+                        break
             else:
-                if not has_been_shut_down:
+                if not has_been_reset:
                     self.set_trim(127)
-                    has_been_shut_down = True
+                    has_been_reset = True
+
+    def close(self):
+        print("Starting shutdown...")
+        self.closing = True  # Tell run_tremolo to terminate
+        self.thread.join()  # Wait for thread to terminate
+        self.set_trim(127)
+        print("...exiting")
+        sys.exit()
 
 
 app = MyApp(application_id="com.example.GtkApplication")
 app.run(sys.argv)
+app.close()
